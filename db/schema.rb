795ff5718e8d58ceb7cbf1221f6fa34c4be8c9bc
ruiji <abcdabcd0517@icloud.com>
2021-07-23 15:19:00 +0900

    画像表示ボタン追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_060504) do
+ActiveRecord::Schema.define(version: 2021_07_23_061231) do
 
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
