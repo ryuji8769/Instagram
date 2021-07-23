94507f648ebaaffb94f42914d76688a895f48c0f
ruiji <abcdabcd0517@icloud.com>
2021-07-23 15:48:12 +0900

    devise初期設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'homes#index'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
+  
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
