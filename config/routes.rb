951aca58283567ef74084c86330428bb7407c3cc
ruiji <abcdabcd0517@icloud.com>
2021-07-24 13:22:22 +0900

    いいね機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+
   get '/' => 'homes#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:      'users/sessions',
   }
+
   resources :posts
-  
-  
+
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show, :create] do
     resources :likes, only: [:create, :destroy]
   end
 
   root 'posts#index'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
