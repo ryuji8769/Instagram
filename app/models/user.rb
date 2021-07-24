73f85c5e6723fd952cd6c1325a6178603b36a192
ruiji <abcdabcd0517@icloud.com>
2021-07-24 17:10:08 +0900

    migrate追加、userModelにアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ApplicationRecord
   # コメント
   has_many :comments, dependent: :destroy
 
+
+  # 通知
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+
   # いいね
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
