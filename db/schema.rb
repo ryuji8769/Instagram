bc46cf6188ff79ecf2f761cace9d33ae10f28c34
ruiji <abcdabcd0517@icloud.com>
2021-07-24 12:04:37 +0900

    migrate追加、アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_070758) do
+ActiveRecord::Schema.define(version: 2021_07_24_030050) do
+
+  create_table "likes", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.string "text"
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_070758) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
