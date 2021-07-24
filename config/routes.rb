a303326d880570f9535bf04b3509d5c755d9d101
ruiji <abcdabcd0517@icloud.com>
2021-07-24 12:08:38 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root '/' => 'homes#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:      'users/sessions',
   }
   
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create] do
+    resources :likes, only: [:create, :destroy]
+  end
+
+  root 'posts#index'
+
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
