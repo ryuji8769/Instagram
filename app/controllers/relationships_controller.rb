15f3bbebe3225d851b15734c52d3b5eeb67b036e
ruiji <abcdabcd0517@icloud.com>
2021-07-24 17:47:19 +0900

    フォローした時だけ通知はこないが、エラーがなくなった

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,12 +3,23 @@ class RelationshipsController < ApplicationController
   end
 
 
+  def create
+    @user = User.find(params[:relationship][:following_id])
+    current_user.follow!(@user)
+    # ここから
+    @user.create_notification_follow!(current_user)
+    # ここまで
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+
   # ユーザーをフォローできるfollowアクションと、ユーザーへのフォロー
   # を外すことができるunfollowアクションを定義
   def follow
     current_user.follow(params[:id])
-    # フォローをしたタイミングで通知レコードを作成
-    @user.create_notification_follow!(current_user)
     redirect_back(fallback_location: root_path)
   end
 
